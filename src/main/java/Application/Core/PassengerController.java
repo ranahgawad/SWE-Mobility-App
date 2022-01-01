e2f69d3fcd2bac21b06a1467a1a4d4c56639f982
Nada Emam <nadaimam8@gmail.com>
2022-01-01 16:38:16 +0200

    functions in passenger controller

--- src/main/java/Application/Core/PassengerController.java
+++ src/main/java/Application/Core/PassengerController.java
@@ -3,10 +3,12 @@ package Application.Core;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import jdk.nashorn.internal.ir.ObjectNode;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 
 @RestController
 public class PassengerController {
@@ -15,25 +17,18 @@ public class PassengerController {
     public PassengerController() {
     }
 
-//
-//    @PostMapping("/requestRide")
-//    public String requestRide(@RequestParam String source,@RequestParam String destination,@RequestParam int num_passengers){
-//        pass.getPassengerModel().requestRide(source, destination, num_passengers);
-//        return "request sent";
-//    }
 
-    @PostMapping("/passenger/requestride")
-    public String requestRide(@RequestBody RideRequest rq)
-    {
-        pass.getPassengerModel().requestRide(rq);
+    @PostMapping("passenger/requestRide")
+    public String requestRide(@RequestParam String source,@RequestParam String destination,@RequestParam int num_passengers){
+        pass.getPassengerModel().requestRide(source, destination, num_passengers);
         return "request sent";
     }
-
-    @PostMapping ("/passenger/login")
+    @PostMapping ("passenger/login")
     boolean login(@RequestBody User user)  {
-         SQLImplementation connection = new SQLImplementation();
-         pass=connection.getCurrentPassenger(user);
-         return Login.perfromLogin(user);
+        SQLImplementation connection = new SQLImplementation();
+        pass=connection.getCurrentPassenger(user);
+        pass.getPassengerModel().printRideOffers();
+        return Login.perfromLogin(user);
     }
 
     @PostMapping ("/passenger/rateDriver/{rating}")
@@ -41,14 +36,18 @@ public class PassengerController {
         pass.getPassengerModel().rateDriver(rating);
         return pass + "rated driver " + rating;
     }
-
-
-
-
-
-
-
-
-
+    @PostMapping ("passenger/acceptOffer/{offerNumber}")
+    public String acceptOffer(@PathVariable int  offerNumber)  {
+        pass.getPassengerModel().acceptOffer(pass.getPassengerModel().getOffer(offerNumber));
+        return pass+"accepted offer from"+pass.getPassengerModel().getOffer(offerNumber).getDriver();
+    }
+    @PostMapping ("passenger/getOffer/{offerNumber}")
+    public String getOffer(@PathVariable int offerNumber){
+        return "offer:" + pass.getPassengerModel().getOffer(offerNumber);
+    }
+    @PostMapping ("passenger/getAllOffers")
+    public ArrayList<Offer> getAllOffers(){
+        return pass.getRideOffers();
+    }
 
 }
